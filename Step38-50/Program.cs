2e823f7d5ceb88cfb365c313aa0dd05d2ca30af2
Holden Wiese <wieseholden@gmail.com>
2020-03-20 22:31:39 -0600

    Add project files.

---
+++ Step38-50/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Step38Through50
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your name?");
+            string username = Console.ReadLine();
+            Console.WriteLine("Hello " + username + "!");
+            Console.Read();
+        }
+    }
+}
---
+++ Step56/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace step56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we're \"Accounting\" on you!");
+            Console.Read();
+        }
+    }
+}
