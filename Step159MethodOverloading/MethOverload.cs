d7bcd2cabad2efe82434501b547eedade70c7819
Holden Wiese <wieseholden@gmail.com>
2020-04-13 17:15:46 -0600

    Update 21 game and add Step 159.

---
+++ Step159MethodOverloading/MethOverload.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Step159MethodOverloading
+{
+    class MethOverload
+    {
+        public static int MathFun(int userInput)
+        {
+            int result = userInput + 5;
+            return result;
+        }
+
+        public static int MathFun(decimal num)
+        {
+            int result = Convert.ToInt32(num * 5);
+            return result;
+        }
+
+        public static int MathFun(string num)
+        {
+            int result = 0;
+            bool tryAgain = true;
+            while (tryAgain)
+            {
+                try
+                {
+                    Console.WriteLine("Write a number you'd like to multiply by 2.");
+                    string userInput = Console.ReadLine();
+                    result = Convert.ToInt32(userInput) * 2;
+                    tryAgain = false;
+                    return result;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("There was an error converting your input to a number. Ensure you write a numeral.\n");
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Step159MethodOverloading/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Step159MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Step 159 | Demonstrating method overloading.\n");
+            Console.WriteLine("Write any int number to increase by 5.");
+            int userSays = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(MethOverload.MathFun(userSays));
+
+            Console.WriteLine("Write any decimal number you want to multiply by 5. " +
+                "The result will be rounded to nearest integer.");
+            decimal userDecimal = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine(MethOverload.MathFun(userDecimal));
+
+            Console.WriteLine("The result of the operation is {0}", MethOverload.MathFun("test"));
+        }
+    }
+}
+
+//1. Create a class. In that class, create a method that will take in an integer, 
+//   do a math operation to it and then return the answer as an integer.
+//2. In the Main() method of the console app, instantiate the class and call the one method, 
+//   passing in an integer.Display the result to the screen.
+//3. Add a second method to the class, with the same name, that will take in a decimal, 
+//   do a different math operation to it and then return the answer as an integer.
+//4. In the Main() method of the console app, instantiate the class and call the second method, 
+//   passing in a decimal. Display the result to the screen.
+//5. Add a third method to the class, with the same name, that will take in a string, 
+//   convert it to an integer if possible, do a different math operation to it and then return the answer as an integer.
+//6. In the Main() method of the console app, instantiate the class and call the third method, 
+//   passing in a string that equates to an integer.Display the result to the screen.
