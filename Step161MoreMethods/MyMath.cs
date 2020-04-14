1d494d32ccc1dbff26db50ef61bd62f09c259442
Holden Wiese <wieseholden@gmail.com>
2020-04-13 18:53:13 -0600

    Step 161

---
+++ Step161MoreMethods/MyMath.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step161MoreMethods
+{
+    class MyMath
+    {
+        public void WeirdMethod(int num1, int num2)
+        {
+            int methodOut = num1 * 10;
+            Console.WriteLine($"The second whole number provided was {num2}.");
+
+            Console.Read();
+        }
+    }
+}
