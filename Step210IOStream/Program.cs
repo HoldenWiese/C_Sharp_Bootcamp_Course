a1bf3e13df76d3d72947bfb8cf9c56dd9250608d
Holden Wiese <wieseholden@gmail.com>
2020-05-03 15:29:29 -0600

    Add Drill 210

---
+++ Step210IOStream/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Step210IOStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamFun();
+        }
+        public static void StreamFun()
+        {
+            Console.WriteLine("What number would you like to log?");
+            string userInput = Console.ReadLine();
+
+            using (StreamWriter sw = new StreamWriter(@"C:\DeleteThisFolder\log.txt", true))
+            {
+                sw.WriteLine(userInput);
+            }
+
+            using (StreamReader sr = new StreamReader(@"C:\DeleteThisFolder\log.txt"))
+            {
+                Console.WriteLine("\nThe following numbers are found in log.txt:");
+                string s;
+                do
+                {
+                    s = sr.ReadLine();
+                    Console.WriteLine(s);
+                } while (s != null);
+            }
+            StreamFun();
+        }
+    }
+}
